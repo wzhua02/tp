f0a703e151e825d2e4ac38c63d2318f5c965ae6d
wzhua02 <woozongghua@gmail.com>
2025-03-09 15:29:06 +0800

    Bug fix: Refactoring in FXML
    Labels in FXML elements are renamed to trainingLocation instead of just
    location. The change in naming specifically in FXML is because
    `location` is a reserved keyword in FXML.
    
    Refer to this S/O discussion on the error
    https://stackoverflow.com/questions/56247046/javafx-java-lang-illegalargumentexception-can-not-set-javafx-scene-control-labe

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -35,7 +35,7 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label phone;
     @FXML
-    private Label address;
+    private Label trainingLocation;
     @FXML
     private Label email;
     @FXML
@@ -50,7 +50,7 @@ public class PersonCard extends UiPart<Region> {
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
-        address.setText(person.getAddress().value);
+        trainingLocation.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
