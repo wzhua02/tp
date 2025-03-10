31dff2ea92f178cea8c02f052ed79c0f37b22777
deseansoh <deseansyc@gmail.com>
2025-03-10 20:30:41 +0800

    Update Messages with Goals

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -41,6 +41,8 @@ public class Messages {
                 .append(person.getPhone())
                 .append("; Email: ")
                 .append(person.getEmail())
+                .append("; Goals: ")
+                .append(person.getGoals())
                 .append("; Address: ")
                 .append(person.getAddress())
                 .append("; Tags: ");
