e9cdc402cc956626e97b670e7773c59c7968c974
deseansoh <deseansyc@gmail.com>
2025-03-15 17:19:11 +0800

    Update logic for HelpCommand

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COMMAND;
+
+import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.Model;
 
 /**
@@ -8,14 +11,84 @@ import seedu.address.model.Model;
 public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions."
+            + "Parameters: "
+            + "[COMMAND]"
+            + "Example: "
+            + COMMAND_WORD + "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
-            + "Example: " + COMMAND_WORD;
+    public static final String GENERIC_HELP_MESSAGE = "Here are a list of basic commands you can use in Fit Flow."
+            + " For more information on each command, type one of the following.\n"
+            + "\nhelp " + PREFIX_COMMAND + AddCommand.COMMAND_WORD
+            + "\nhelp " + PREFIX_COMMAND + ListCommand.COMMAND_WORD
+            + "\nhelp " + PREFIX_COMMAND + EditCommand.COMMAND_WORD
+            + "\nhelp " + PREFIX_COMMAND + FindCommand.COMMAND_WORD
+            + "\nhelp " + PREFIX_COMMAND + DeleteCommand.COMMAND_WORD
+            + "\nhelp " + PREFIX_COMMAND + ClearCommand.COMMAND_WORD
+            + "\nhelp " + PREFIX_COMMAND + ExitCommand.COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    private final String commandRequested;
+
+    /**
+     * Creates a HelpCommand with the specified commandRequested
+     */
+    public HelpCommand(String commandRequested) {
+        this.commandRequested = commandRequested;
+    }
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        String helpMessage;
+
+        switch (commandRequested.toLowerCase()) {
+        case AddCommand.COMMAND_WORD:
+            helpMessage = AddCommand.MESSAGE_USAGE;
+            break;
+        case ClearCommand.COMMAND_WORD:
+            helpMessage = ClearCommand.MESSAGE_USAGE;
+            break;
+        case DeleteCommand.COMMAND_WORD:
+            helpMessage = DeleteCommand.MESSAGE_USAGE;
+            break;
+        case EditCommand.COMMAND_WORD:
+            helpMessage = EditCommand.MESSAGE_USAGE;
+            break;
+        case ExitCommand.COMMAND_WORD:
+            helpMessage = ExitCommand.MESSAGE_USAGE;
+            break;
+        case FindCommand.COMMAND_WORD:
+            helpMessage = FindCommand.MESSAGE_USAGE;
+            break;
+        case ListCommand.COMMAND_WORD:
+            helpMessage = ListCommand.MESSAGE_USAGE;
+            break;
+        default:
+            helpMessage = GENERIC_HELP_MESSAGE;
+            break;
+        }
+
+        return new CommandResult(helpMessage);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof HelpCommand)) {
+            return false;
+        }
+
+        HelpCommand otherAddCommand = (HelpCommand) other;
+        return commandRequested.equals(otherAddCommand.commandRequested);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("commandRequested", commandRequested)
+                .toString();
     }
 }
