b1e46a10991661f6b2ba3aa2e2124e1d269c35e0
deseansoh <deseansyc@gmail.com>
2025-03-15 17:05:55 +0800

    Update ExitCommand MESSAGE_USAGE

--- src/main/java/seedu/address/logic/commands/ExitCommand.java
+++ src/main/java/seedu/address/logic/commands/ExitCommand.java
@@ -8,7 +8,10 @@ import seedu.address.model.Model;
 public class ExitCommand extends Command {
 
     public static final String COMMAND_WORD = "exit";
-
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Closes the application.\n\n"
+            + "Format: "
+            + COMMAND_WORD + "\n\n"
+            + "Example: " + COMMAND_WORD;
     public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Address Book as requested ...";
 
     @Override
