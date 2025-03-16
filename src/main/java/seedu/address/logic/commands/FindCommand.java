53347f3ae5699b22b0a1c5bbbc9d5005af1030f3
deseansoh <deseansyc@gmail.com>
2025-03-16 23:00:07 +0800

    Update FindCommand MESSAGE_USAGE

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -16,7 +16,8 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": View a client in the client list whose names contain "
-            + "any of the specified keywords (case-insensitive) and displays them as a list with index numbers.\n\n"
+            + "any of the specified keywords (case-insensitive) and displays them as a list with index numbers. This"
+            + "list contains only client names and their respective phone numbers.\n\n"
             + "Format: "
             + COMMAND_WORD + " [KEYWORD]\n\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
